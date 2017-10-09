1e619f9bca91d25c84e6be07205a180b432f725a
Kevin <stradtkt22@gmail.com>
2017-10-09 16:46:05 -0400

    Added testing

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 //need to use http with socket.io
 const express = require('express');
 const socketIO = require('socket.io');
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -11,13 +12,17 @@ app.use(express.static(publicPath));
 var io = socketIO(server);
 io.on('connection', (socket) => {
     console.log('New user connected');
-    socket.emit('newMessage', {
-        from: 'Kevin',
-        text: 'See you then',
-        createdAt: 123123
-    });
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
+        socket.broadcast.emit('newMessage', {
+                from: message.from,
+                text: message.text,
+                createdAt: new Date().getTime()
+        });
+        //sends to everyone but this socket
         });
     socket.on('disconnect', () => {
         console.log('User was Disconnected');
---
+++ server/utils/message.test.js
@@ -0,0 +1,12 @@
+var expect = require('expect');
+var {generateMessage} = require('./message');
+describe('generateMessage', () => {
+    it('should generate the correct message object', () => {
+        var from = 'Jen';
+        var text = 'Some Message';
+        var message = generateMessage(from, text);
+
+        expect(message.createdAt).toBeA('number');
+        expect(message).toInclude({from,text})
+    });
+});
