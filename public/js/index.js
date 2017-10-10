1e54d030c7bed15d7b2744bba118e9284c16191a
Kevin <stradtkt22@gmail.com>
2017-10-10 11:48:58 -0400

    Added form input to the chat app

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,6 @@
 var socket = io();
 socket.on('connect', function() {
     console.log('Connected to server');
-
-socket.emit('createMessage', {
-    from: 'Kevin',
-    text: 'This is a message to be made'
-});
 });
 socket.on('disconnect', function() {
     console.log('Disconnected from server');
@@ -15,4 +10,22 @@ socket.on('newEmail', function(email) {
 });
 socket.on('newMessage', function(message) {
     console.log('newMessage', message);
-})
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}:  ${message.text}`);
+    jQuery('#messages').append(li);
+});
+socket.emit('createMessage', {
+    from: 'Frank',
+    text: 'HI'
+}, function(data) {
+    console.log('Got it', data);
+});
+jQuery('#message-form').on('submit', function(e) {
+    e.preventDefault();
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, function() {
+
+    });
+});
--- server/server.js
+++ server/server.js
@@ -15,13 +15,15 @@ io.on('connection', (socket) => {
     socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
-    socket.on('createMessage', (message) => {
+    socket.on('createMessage', (message,callback) => {
         console.log('createMessage', message);
-        socket.broadcast.emit('newMessage', {
-                from: message.from,
-                text: message.text,
-                createdAt: new Date().getTime()
-        });
+        io.emit('newMessage', generateMessage(message.from, message.text));
+        callback('This is a message from the server');
+        // socket.broadcast.emit('newMessage', {
+        //         from: message.from,
+        //         text: message.text,
+        //         createdAt: new Date().getTime()
+        // });
         //sends to everyone but this socket
         });
     socket.on('disconnect', () => {
